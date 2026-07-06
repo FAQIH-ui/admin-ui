be2847922f943dee03eacca0da2aeb1125cdd434
Faqih <faqihrafiq07@gmail.com>
2026-07-06 20:31:42 +0700

    Frontend-Backend Integration

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Parent from "./latihan/Parent.jsx";
-import { CounterContextProvider } from "./context/counterContext.jsx";
 import { ThemeContextProvider } from "./context/themeContext.jsx";
+import { AuthContextProvider } from "./context/authContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContextProvider>
+    <AuthContextProvider>
+      <ThemeContextProvider>
         <App />
-    </ThemeContextProvider>
-  </React.StrictMode>,
+      </ThemeContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
 );
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useEffect, useState} from 'react'
 import MainLayout from '../components/Layouts/MainLayout';
 import Card from '../components/Elements/Card';
 import CardBalance from '../components/Fragments/CardBalance';
@@ -7,9 +7,31 @@ import CardUpcomingBill from '../components/Fragments/CardUpcomingBill';
 import CardRecentTransaction from '../components/Fragments/CardRecentTransaction';
 import CardStatistic from '../components/Fragments/CardStatistic';
 import CardExpenseBreakdown from '../components/Fragments/CardExpenseBreakdown'; 
-import { transactions, bills, expensesBreakdowns, balances, goals, expensesStatistics } from '../data'; 
+import { transactions, bills, expensesBreakdowns, balances, goals, expensesStatistics } from '../data';
+import { goalService } from '../services/dataService.jsx';
+import { AuthContext } from '../context/authContext.jsx';
 
 function dashboard() {
+  const [goals, setGoals] = useState({});
+
+  const fetchGoals = async () => {
+    try {
+      const data = await goalService();
+      setGoals(data);
+    } catch (err) {
+      console.error("Gagal mengambil data goals:", err);
+      if (err.status === 401) {
+        logout();
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+  
+  console.log(goals);
+  
   return (
     <>
       <MainLayout>
